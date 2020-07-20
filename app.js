b7ef3c216e7647864988f345eefe080392fa0e8c
Ali Ahmad <alislearning123@gmail.com>
2020-07-20 16:08:56 -0400

    Fix Price Value Issue When Editing Posts

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-var express       = require("express"),
-    app           = express(),
-    bodyParser    = require("body-parser"),
-    mongoose      = require("mongoose"),
-    Shoe          = require("./models/shoe"),
-    Comment       = require("./models/comment"),
-    User          = require("./models/user"),
-    seedDB        = require("./seeds"),
-    passport      = require("passport"),
-    LocalStrategy = require("passport-local"),
-	methodOverride = require("method-override"),
-	flash = require("connect-flash");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  Shoe = require("./models/shoe"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  seedDB = require("./seeds"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  flash = require("connect-flash");
 
 var commentRoutes = require("./routes/comments"),
-	shoeRoutes = require("./routes/shoes"),
-	indexRoutes = require("./routes/index");
+  shoeRoutes = require("./routes/shoes"),
+  indexRoutes = require("./routes/index");
 
-const session = require('express-session'),
-      MongoStore = require('connect-mongo')(session);
-
+const session = require("express-session"),
+  MongoStore = require("connect-mongo")(session);
 
 // mongoose.connect("mongodb://localhost:27017/xtocks", {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect("mongodb+srv://aahmad4:aahmad4@cluster0.mfoxo.mongodb.net/xtocks?retryWrites=true&w=majority", 
-	{
-		useNewUrlParser: true, 
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false
-	}
+mongoose.connect(
+  "mongodb+srv://aahmad4:aahmad4@cluster0.mfoxo.mongodb.net/xtocks?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
 );
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -37,11 +37,13 @@ app.use(flash());
 // seedDB();
 
 // Passport Configuration
-app.use(require("express-session")({
-	secret: "Something random lol",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    secret: "Something random lol",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -49,14 +51,14 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
-	next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
-app.use("/", indexRoutes); 
+app.use("/", indexRoutes);
 // Appends /shoes infront of all shoe routes
 app.use("/shoes", shoeRoutes);
 app.use("/shoes/:id/comments", commentRoutes);
