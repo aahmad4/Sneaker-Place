e7bd470e12b5bcc222e5c1521e37df69e2316c80
Ali Ahmad <alislearning123@gmail.com>
2021-04-29 02:07:54 -0400

    Add Controllers & Install Axios & Re-arrange Index Routes

---
+++ controllers/index.controller.js
@@ -0,0 +1,80 @@
+import asyncHandler from "express-async-handler";
+import passport from "passport";
+import axios from "axios";
+import User from "../models/user.model.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const homePage = (req, res) => {
+  res.redirect("/shoes");
+};
+
+const feedPage = asyncHandler(async (req, res) => {
+  const { data } = await axios.get(
+    "https://v1-sneakers.p.rapidapi.com/v1/sneakers",
+    {
+      params: { limit: "100" },
+      headers: {
+        "x-rapidapi-key": process.env.RAPIDAPIKEY,
+        "x-rapidapi-host": "v1-sneakers.p.rapidapi.com",
+      },
+    }
+  );
+
+  res.render("feed", { parsedData: data });
+});
+
+const registrationForm = (req, res) => {
+  if (req.isAuthenticated()) {
+    res.redirect("/shoes");
+  } else {
+    res.render("register");
+  }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+
+  const newUser = new User({ username: username });
+
+  User.register(newUser, password, (err, user) => {
+    if (err) {
+      req.flash("error", err.message);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, () => {
+      req.flash("success", "Welcome " + user.username);
+      res.redirect("/shoes");
+    });
+  });
+});
+
+const loginForm = (req, res) => {
+  if (req.isAuthenticated()) {
+    res.redirect("/shoes");
+  } else {
+    res.render("login");
+  }
+};
+
+const logoutUser = (req, res) => {
+  req.logout();
+  req.flash("success", "Logged you out!");
+  res.redirect("/shoes");
+};
+
+const loginUser = passport.authenticate("local", {
+  successRedirect: "/shoes",
+  failureRedirect: "/login",
+});
+
+export {
+  homePage,
+  feedPage,
+  registrationForm,
+  registerUser,
+  loginForm,
+  logoutUser,
+  loginUser,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import express from "express";
-const router = express.Router();
-import passport from "passport";
-import request from "request";
-import User from "../models/user.model.js";
-import dotenv from "dotenv";
-
-dotenv.config();
+import {
+  homePage,
+  feedPage,
+  registrationForm,
+  registerUser,
+  loginForm,
+  logoutUser,
+  loginUser,
+} from "../controllers/index.controller.js";
 
 const isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -15,78 +17,12 @@ const isLoggedIn = (req, res, next) => {
   res.redirect("/login");
 };
 
-router.get("/", (req, res) => {
-  res.redirect("/shoes");
-});
+const router = express.Router();
 
-router.get("/feed", (req, res) => {
-  const options = {
-    method: "GET",
-    url: "https://v1-sneakers.p.rapidapi.com/v1/sneakers",
-    qs: { limit: "100" },
-    headers: {
-      "x-rapidapi-key": process.env.RAPIDAPIKEY,
-      "x-rapidapi-host": "v1-sneakers.p.rapidapi.com",
-      useQueryString: true,
-    },
-  };
-
-  request(options, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      const parsedData = JSON.parse(body);
-      res.render("feed", { parsedData: parsedData });
-    }
-  });
-});
-
-// Show register form
-router.get("/register", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.redirect("/shoes");
-  } else {
-    res.render("register");
-  }
-});
-
-// Sign Up Logic
-router.post("/register", (req, res) => {
-  const newUser = new User({ username: req.body.username });
-  User.register(newUser, req.body.password, (err, user) => {
-    if (err) {
-      req.flash("error", err.message);
-      return res.render("register");
-    }
-    passport.authenticate("local")(req, res, () => {
-      req.flash("success", "Welcome " + user.username);
-      res.redirect("/shoes");
-    });
-  });
-});
-
-// Show login form
-router.get("/login", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.redirect("/shoes");
-  } else {
-    res.render("login");
-  }
-});
-
-// Handle login logic
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/shoes",
-    failureRedirect: "/login",
-  }),
-  (req, res) => {}
-);
-
-// Logout route
-router.get("/logout", (req, res) => {
-  req.logout();
-  req.flash("success", "Logged you out!");
-  res.redirect("/shoes");
-});
+router.route("/").get(homePage);
+router.route("/feed").get(feedPage);
+router.route("/register").get(registrationForm).post(registerUser);
+router.route("/login").get(loginForm).post(loginUser);
+router.route("/logout").get(logoutUser);
 
 export default router;
