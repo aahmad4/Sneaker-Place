6e3f8686ae39274ae504b8ec2e71bd149ae0e8e1
Ali Ahmad <alislearning123@gmail.com>
2021-04-29 00:48:08 -0400

    Remove Extra Imports & Uninstall Legacy Packages

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 import express from "express";
-const app = express();
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import Shoe from "./models/shoe.js";
-import Comment from "./models/comment.js";
 import User from "./models/user.js";
-import seedDB from "./seeds.js";
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import methodOverride from "method-override";
 import flash from "connect-flash";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import dotenv from "dotenv";
 
 import commentRoutes from "./routes/comments.js";
 import shoeRoutes from "./routes/shoes.js";
 import indexRoutes from "./routes/index.js";
 
 import session from "express-session";
-import MongoStore from "connect-mongo";
 
 const url = process.env.DATABASEURL || "mongodb://localhost:27017/xtocks";
 
@@ -29,17 +22,18 @@ mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
 });
-app.use(bodyParser.urlencoded({ extended: true }));
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-// Seed the database
-// seedDB();
 
-// Passport Configuration
 app.use(
   session({
     secret: "Something random lol",
@@ -65,8 +59,8 @@ app.use("/", indexRoutes);
 app.use("/shoes", shoeRoutes);
 app.use("/shoes/:id/comments", commentRoutes);
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+const port = 8000 || process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
