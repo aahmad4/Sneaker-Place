40e5f9d2d66172db3eb158c7b60662867f626740
Ali Ahmad <alislearning123@gmail.com>
2021-04-29 01:36:49 -0400

    Update Seeder File To Use Modern Async / Await Syntax

--- seeder.js
+++ seeder.js
@@ -1,68 +1,48 @@
-import mongoose from "mongoose";
 import Shoe from "./models/shoe.model.js";
 import Comment from "./models/comment.model.js";
 
-const data = [
+const shoes = [
   {
-    name: "Cloud's Rest",
-    image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+    name: "ZX 2K Boost Shoes",
+    image:
+      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/576317cd320d4f73b4ffab6d002dadea_9366/ZX_2K_Boost_Shoes_White_FX9519_01_standard.jpg",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    price: "100",
   },
   {
-    name: "Desert Mesa",
-    image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+    name: "Nike Adapt Huarache Yellow",
+    image:
+      "https://c.static-nike.com/a/images/t_prod_ss/w_640,c_limit,q_auto,f_auto/vrl9bh3apdvbswafihti/nike-adapt-huarache-opti-yellow-release-date.jpg",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    price: "100",
   },
   {
-    name: "Canyon Floor",
-    image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+    name: "Better Human Races",
+    image:
+      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/f3b0d4f1c58748508ff7abb100b54fec_9366/Lite_Racer_Adapt_3.0_Shoes_Blue_FX8805_FX8805_04_standard.jpg",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    price: "100",
   },
 ];
 
-const seedDB = () => {
-  //Remove all shoes
-  Shoe.deleteMany({}, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log("removed shoes!");
-    Comment.deleteMany({}, (err) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log("removed comments!");
-      // add a few shoes
-      data.forEach((seed) => {
-        Shoe.create(seed, (err, shoe) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("added a shoe");
-            //create a comment
-            Comment.create(
-              {
-                text: "This place is great, but I wish there was internet",
-                author: "Homer",
-              },
-              (err, comment) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  shoe.comments.push(comment);
-                  shoe.save();
-                  console.log("Created new comment");
-                }
-              }
-            );
-          }
-        });
-      });
-    });
+const seedDB = async () => {
+  await Shoe.deleteMany();
+  await Comment.deleteMany();
+
+  const createdShoes = await Shoe.insertMany(shoes);
+
+  const newComment = await Comment.create({
+    text: "This is the best shoe!",
+    author: { username: "Homer" },
   });
+
+  for (const shoe of createdShoes) {
+    shoe.comments.push(newComment);
+    await shoe.save();
+  }
 };
 
 export default seedDB;
